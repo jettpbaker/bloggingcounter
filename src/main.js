e8e57dd5913804e98499d5ccc5c66ecbcbac6127
jettpbaker <jett.p.baker@gmail.com>
2025-06-07 14:02:40 +1000

    added vuetify

--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
-import { createApp } from 'vue';
-import { clerkPlugin } from '@clerk/vue';
-import App from './App.vue';
-import router from './router';
+import { createApp } from 'vue'
+import { clerkPlugin } from '@clerk/vue'
+import App from './App.vue'
+import router from './router'
 
 // Clerk publishable key does not need to be kept secret
-const PUBLISHABLE_KEY = 'pk_test_dGVuZGVyLXdhc3AtODAuY2xlcmsuYWNjb3VudHMuZGV2JA';
+const PUBLISHABLE_KEY = 'pk_test_dGVuZGVyLXdhc3AtODAuY2xlcmsuYWNjb3VudHMuZGV2JA'
 
-const app = createApp(App);
-app.use(clerkPlugin, { publishableKey: PUBLISHABLE_KEY });
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-app.use(router);
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
-app.mount('#app');
+const app = createApp(App)
+app.use(clerkPlugin, { publishableKey: PUBLISHABLE_KEY })
+app.use(vuetify)
+app.use(router)
+
+app.mount('#app')
