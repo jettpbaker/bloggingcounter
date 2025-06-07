dc6e672951ccf8723a2d09a694b05c2d935c65e0
jettpbaker <jett.p.baker@gmail.com>
2025-06-07 16:03:41 +1000

    can now seed DB

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import { Hono } from 'hono'
 import pong from './routes/pong'
 import posts from './routes/posts'
+import seed from './routes/seed'
 
 const app = new Hono()
 
 app.route('/api/pong', pong)
 app.route('/api/posts', posts)
+app.route('/api/seed', seed)
 
 app.get('/api', (c) => {
   console.log('Hello From Hono!')
