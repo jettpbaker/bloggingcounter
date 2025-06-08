f6e5f693fa61b1b0f66e32c922939b593d29c8a9
jettpbaker <jett.p.baker@gmail.com>
2025-06-08 13:50:35 +1000

    initial working query

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <script setup>
 import { useUser } from '@clerk/vue'
+import { useQuery } from '@tanstack/vue-query'
 
 const { isSignedIn, user, isLoaded } = useUser()
+
+const { isPending, isError, data, error } = useQuery({
+  queryKey: ['posts'],
+  queryFn: async () => {
+    const response = await fetch('/api/posts')
+    const data = await response.json()
+    return data.posts
+  },
+})
 </script>
 
 <template>
   <main>
-    <div v-if="!isLoaded">Loading...</div>
+    <div v-if="!isLoaded">Loading User...</div>
 
     <div v-else-if="isSignedIn">
       <h1>Hello {{ user?.fullName }}</h1>
 
       <div style="margin-top: 20px">
         <h2>Home</h2>
+
+        <div v-if="isPending">Loading Posts...</div>
+        <div v-else-if="isError">Error: {{ error.message }}</div>
+
+        <ul v-else>
+          <li v-for="post in data" :key="post.id">
+            <h3>{{ post.title }}</h3>
+            <p>{{ post.content }}</p>
+          </li>
+        </ul>
       </div>
     </div>
 
